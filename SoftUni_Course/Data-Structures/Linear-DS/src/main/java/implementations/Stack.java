686733a6b570c89676e47cdee10237f0edae3094
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-08 23:49:15 +0300

    Implementing the Stack DS with Nodes

--- SoftUni_Course/Data-Structures/Linear-DS/src/main/java/implementations/Stack.java
+++ SoftUni_Course/Data-Structures/Linear-DS/src/main/java/implementations/Stack.java
@@ -6,33 +6,89 @@ import java.util.Iterator;
 
 public class Stack<E> implements AbstractStack<E> {
 
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E>{
+        private E value;
+        private Node<E> next;
+        Node(E element){
+            this.value = element;
+        }
+    }
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
+
     @Override
     public void push(E element) {
 
+        Node<E> toInsert = new Node<>(element);
+
+        // Set the 'next' reference of the new node to point to the current top of the stack
+        toInsert.next = top;
+
+        // Update the 'top' reference to point to the newly inserted node,
+        // making it the new top of the stack
+        this.top = toInsert;
+
+
+        this.size++;
     }
 
     @Override
     public E pop() {
-        return null;
+        ensureNotEmpty();
+
+        Node<E> lastElTmp = top;
+        top = lastElTmp.next;
+
+        this.size--;
+        return lastElTmp.value;
+    }
+
+    private void ensureNotEmpty() {
+        if(this.isEmpty()){
+            throw new IllegalStateException();
+        }
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNotEmpty();
+
+        return this.top.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size==0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+
+            private Node<E> current = top;
+            @Override
+            public boolean hasNext() {
+                return this.current !=null;
+            }
+
+            @Override
+            public E next() {
+                E value = this.current.value;
+                this.current = this.current.next;
+
+                return value;
+            }
+        };
     }
 }
