5d181be0decc88516bef28a0e26ba108b4901613
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-09 12:51:08 +0300

    Adding new tests

--- SoftUni_Course/Data-Structures/Linear-DS/src/test/java/implementations/DequeTest.java
+++ SoftUni_Course/Data-Structures/Linear-DS/src/test/java/implementations/DequeTest.java
@@ -1,7 +1,6 @@
 package implementations;
 
 import static org.junit.Assert.*;
-
 import interfaces.Deque;
 import org.junit.*;
 
@@ -15,146 +14,105 @@ public class DequeTest {
     }
 
     @Test
-    public void testAdd() {
-        deque.add(1);
-        deque.add(2);
-        assertEquals(2, deque.size());
-        assertEquals(Integer.valueOf(1), deque.get(0));
-        assertEquals(Integer.valueOf(2), deque.get(1));
-    }
-
-    @Test
-    public void testOffer() {
-        deque.offer(1);
-        deque.offer(2);
-        assertEquals(2, deque.size());
-        assertEquals(Integer.valueOf(1), deque.get(0));
-        assertEquals(Integer.valueOf(2), deque.get(1));
-    }
-
-    @Test
     public void testAddFirst() {
         deque.addFirst(1);
-        deque.addFirst(2);
-        assertEquals(2, deque.size());
-        assertEquals(Integer.valueOf(2), deque.get(0));
-        assertEquals(Integer.valueOf(1), deque.get(1));
+        assertEquals(1, deque.size());
+        assertEquals(Integer.valueOf(1), deque.peek());
     }
 
     @Test
     public void testAddLast() {
         deque.addLast(1);
-        deque.addLast(2);
-        assertEquals(2, deque.size());
-        assertEquals(Integer.valueOf(1), deque.get(0));
-        assertEquals(Integer.valueOf(2), deque.get(1));
-    }
-
-    @Test
-    public void testPush() {
-        deque.push(1);
-        deque.push(2);
-        assertEquals(2, deque.size());
-        assertEquals(Integer.valueOf(2), deque.get(0));
-        assertEquals(Integer.valueOf(1), deque.get(1));
-    }
-
-    @Test
-    public void testInsert() {
-        deque.add(1);
-        deque.insert(1, 2);
-        assertEquals(2, deque.size());
-        assertEquals(Integer.valueOf(1), deque.get(0));
-        assertEquals(Integer.valueOf(2), deque.get(1));
-    }
-
-    @Test
-    public void testSet() {
-        deque.add(1);
-        deque.set(0, 2);
         assertEquals(1, deque.size());
-        assertEquals(Integer.valueOf(2), deque.get(0));
+        assertEquals(Integer.valueOf(1), deque.peek());
+    }
+
+    @Test
+    public void testAdd() {
+        deque.add(1);
+        assertEquals(1, deque.size());
+        assertEquals(Integer.valueOf(1), deque.peek());
+    }
+
+    @Test
+    public void testOffer() {
+        deque.offer(1);
+        assertEquals(1, deque.size());
+        assertEquals(Integer.valueOf(1), deque.peek());
+    }
+
+    @Test
+    public void testRemoveFirst() {
+        deque.addFirst(1);
+        assertEquals(Integer.valueOf(1), deque.removeFirst());
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    public void testRemoveLast() {
+        deque.addLast(1);
+        assertEquals(Integer.valueOf(1), deque.removeLast());
+        assertTrue(deque.isEmpty());
     }
 
     @Test
     public void testPeek() {
         deque.add(1);
-        deque.add(2);
         assertEquals(Integer.valueOf(1), deque.peek());
+        assertFalse(deque.isEmpty());
     }
 
     @Test
     public void testPoll() {
         deque.add(1);
-        deque.add(2);
         assertEquals(Integer.valueOf(1), deque.poll());
-        assertEquals(1, deque.size());
+        assertTrue(deque.isEmpty());
     }
 
     @Test
     public void testPop() {
         deque.add(1);
-        deque.add(2);
         assertEquals(Integer.valueOf(1), deque.pop());
-        assertEquals(1, deque.size());
+        assertTrue(deque.isEmpty());
     }
 
     @Test
     public void testGetByIndex() {
         deque.add(1);
-        deque.add(2);
         assertEquals(Integer.valueOf(1), deque.get(0));
-        assertEquals(Integer.valueOf(2), deque.get(1));
-    }
-
-    @Test
-    public void testGetByObject() {
-        deque.add(1);
-        deque.add(2);
-        assertEquals(Integer.valueOf(1), deque.get(1));
     }
 
     @Test
     public void testRemoveByIndex() {
         deque.add(1);
-        deque.add(2);
         assertEquals(Integer.valueOf(1), deque.remove(0));
-        assertEquals(1, deque.size());
+        assertTrue(deque.isEmpty());
     }
 
     @Test
     public void testRemoveByObject() {
         deque.add(1);
-        deque.add(2);
-        assertEquals(1, deque.size());
-    }
-
-    @Test
-    public void testRemoveFirst() {
-        deque.add(1);
-        deque.add(2);
-        assertEquals(Integer.valueOf(1), deque.removeFirst());
-        assertEquals(1, deque.size());
-    }
-
-    @Test
-    public void testRemoveLast() {
-        deque.add(1);
-        deque.add(2);
-        assertEquals(Integer.valueOf(2), deque.removeLast());
-        assertEquals(1, deque.size());
+        assertTrue(deque.isEmpty());
     }
 
     @Test
     public void testSize() {
+        assertEquals(0, deque.size());
         deque.add(1);
         deque.add(2);
         assertEquals(2, deque.size());
     }
 
     @Test
+    public void testIsEmpty() {
+        assertTrue(deque.isEmpty());
+        deque.add(1);
+        assertFalse(deque.isEmpty());
+    }
+
+    @Test
     public void testCapacity() {
-        assertEquals(0, deque.capacity());
+        assertEquals(7, deque.capacity());
     }
 
     @Test
@@ -166,9 +124,13 @@ public class DequeTest {
     }
 
     @Test
-    public void testIsEmpty() {
-        assertTrue(deque.isEmpty());
+    public void testIterator() {
         deque.add(1);
-        assertFalse(deque.isEmpty());
+        deque.add(2);
+        int sum = 0;
+        for (Integer i : deque) {
+            sum += i;
+        }
+        assertEquals(3, sum);
     }
 }
