640cd198c9a81a027e6d5a80152d3e350b30924c
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-11 20:46:30 +0300

    Implementing creating tree methods including  the tree factory

--- SoftUni_Course/Data-Structures/Trees-Representation-Traversal/src/main/java/implementations/SecondTree.java
+++ SoftUni_Course/Data-Structures/Trees-Representation-Traversal/src/main/java/implementations/SecondTree.java
@@ -2,28 +2,45 @@ package implementations;
 
 import interfaces.AbstractSecondTree;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecondTree<E> implements AbstractSecondTree<E> {
 
+    private E key;
+    private SecondTree<E> parent;
+    private List<SecondTree<E>> children;
+
+    public SecondTree(E value, SecondTree<E>... subtrees) {
+        this.key = value;
+        this.parent = null;
+        this.children = new ArrayList<>();
+
+        for (SecondTree<E> subtree : subtrees) {
+            this.children.add(subtree);
+            setParent(this);
+        }
+    }
+
     @Override
     public void setParent(SecondTree<E> parent) {
-
+        this.parent=parent;
     }
 
     @Override
     public void addChild(SecondTree<E> child) {
+        this.children.add(child);
 
     }
 
     @Override
     public SecondTree<E> getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public E getKey() {
-        return null;
+        return this.key;
     }
 
     @Override
--- SoftUni_Course/Data-Structures/Trees-Representation-Traversal/src/main/java/implementations/TreeFactory.java
+++ SoftUni_Course/Data-Structures/Trees-Representation-Traversal/src/main/java/implementations/TreeFactory.java
@@ -1,5 +1,6 @@
 package implementations;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -11,19 +12,42 @@ public class TreeFactory {
     }
 
     public SecondTree<Integer> createTreeFromStrings(String[] input) {
-        return null;
+        // Here we take input such as "7 19"
+        for (String params : input) {
+            int[] keys = Arrays.stream(params.split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            int parentKey = keys[0];
+            int childKey = keys[1];
+
+            // Create the parent if no such exists and make the relations between them
+            addEdge(parentKey,childKey);
+
+        }
+        return this.getRoot();
     }
 
     private SecondTree<Integer> getRoot() {
+        for (SecondTree<Integer> value : nodesByKeys.values()) {
+            if(value.getParent()==null){
+                return value;
+            }
+        }
         return null;
     }
 
     public SecondTree<Integer> createNodeByKey(int key) {
-        return null;
+        this.nodesByKeys.putIfAbsent(key,new SecondTree<>(key));
+       return this.nodesByKeys.get(key);
     }
 
     public void addEdge(int parent, int child) {
+        SecondTree<Integer> parentByKey = this.createNodeByKey(parent);
+        SecondTree<Integer> childByKey = createNodeByKey(child);
 
+        childByKey.setParent(parentByKey);
+        parentByKey.addChild(childByKey);
     }
 }
 
