10aa35335d9f96953ab2a927a95f7017b5110734
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-13 18:45:41 +0300

    Implementing a MaxHeap Using ArrayList
     A completed binary tree ( we insert top to bottom , left to right)  for which parent >= children
      We need our elements to be comparable

--- SoftUni_Course/Data-Structures/Heaps-BST/src/main/java/implementations/MaxHeap.java
+++ SoftUni_Course/Data-Structures/Heaps-BST/src/main/java/implementations/MaxHeap.java
@@ -5,21 +5,56 @@ import interfaces.Heap;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+/*
+  A completed binary tree ( we insert top to bottom , left to right)  for which parent >= children
+  We need our elements to be comparable
+ */
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
+    private List<E> elements;
 
-    @Override
-    public int size() {
-        return 0;
+    public MaxHeap(){
+        this.elements = new ArrayList<>();
     }
 
     @Override
-    public void add(E element) {
+    public int size() {
+        return elements.size();
+    }
 
+    /*
+    By adding elements to the list sequentially and performing heapify operations to maintain the heap property after each addition,
+    the implementation naturally achieves the "top to bottom, left to right" insertion pattern characteristic of a heap,
+    because we use a list and we add at the end( the end layer of the tree)
+     */
+    @Override
+    public void add(E element) {
+        this.elements.add(element);
+        this.heapifyUp(this.size()-1);
+
+    }
+
+    private void heapifyUp(int index) {
+        int parentIndex = getParentIndex(index);
+        while (index > 0 && isLess(index,parentIndex)){
+            Collections.swap(this.elements,index,parentIndex);
+            index=parentIndex;
+        }
+    }
+
+    private boolean isLess(int childIndex, int parentIndex) {
+        return getAt(childIndex).compareTo(getAt(parentIndex)) > 0;
+    }
+
+    private E getAt(int index){
+        return this.elements.get(index);
+    }
+
+    private int getParentIndex(int index) {
+        return (index-1 )/ 2;
     }
 
     @Override
     public E peek() {
-        return null;
+        return this.elements.get(0);
     }
 }
