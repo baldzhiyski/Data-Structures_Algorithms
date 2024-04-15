0fe04da23e51c14ceafc234da5e85dbdc79f6ec6
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-15 14:38:28 +0300

    Implementing CookiesProblem

--- SoftUni_Course/Data-Structures/Heaps-BST/src/main/java/solutions/CookiesProblem.java
+++ SoftUni_Course/Data-Structures/Heaps-BST/src/main/java/solutions/CookiesProblem.java
@@ -1,7 +1,29 @@
 package solutions;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class CookiesProblem {
-    public Integer solve(int k, int[] cookies) {
-        return null;
+    public Integer solve(int requiredSweetness, int[] cookiesSweetness) {
+        Queue<Integer> cookies = new PriorityQueue<>();
+
+        for (int sweetness : cookiesSweetness) {
+            cookies.add(sweetness);
+        }
+        int currentMinSweetness = cookies.peek();
+        int steps = 0;
+        while (currentMinSweetness<requiredSweetness && cookies.size()>1){
+            int leastSweetness = cookies.poll();
+            int secondLeastSweetness = cookies.poll();
+
+            int combined = leastSweetness + 2*secondLeastSweetness;
+            cookies.add(combined);
+
+            currentMinSweetness = cookies.peek();
+            steps++;
+        }
+
+
+        return currentMinSweetness>requiredSweetness ? steps : -1;
     }
 }
