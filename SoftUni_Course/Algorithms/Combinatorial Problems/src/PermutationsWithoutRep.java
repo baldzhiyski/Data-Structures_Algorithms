bbb85a6448c7c483990d4c1d4cdcaac24ac41118
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-27 13:31:18 +0300

    Optimizing the solution of permutations

--- SoftUni_Course/Algorithms/Combinatorial Problems/src/PermutationsWithoutRep.java
+++ SoftUni_Course/Algorithms/Combinatorial Problems/src/PermutationsWithoutRep.java
@@ -1,8 +1,6 @@
 
 public class PermutationsWithoutRep {
-    public  static String[] elements = {"A","B","C","D"};
-    public static String[] permutations = new String[elements.length];
-    public static boolean[] usedPos = new boolean[elements.length];
+    public  static String[] elements = {"A","B","C"};
     public static void main(String[] args) {
 
         permute(0);
@@ -13,19 +11,23 @@ public class PermutationsWithoutRep {
             printPermutation();
             return;
         }
-        for (int i = 0; i < elements.length; i++) {
-            if(!usedPos[i]) {
-                usedPos[i] = true;
-                permutations[index] = elements[i];
-                permute(index+1);
-                usedPos[i] = false;
-            }
+        permute(index + 1);
+        for (int i = index + 1; i < elements.length; i++) {
+            swap(index,i);
+            permute(index + 1);
+            swap(index,i);
         }
 
 
     }
 
+    private static void swap(int first, int second) {
+        String temp = elements[first];
+        elements[first] = elements[second];
+        elements[second] = temp;
+    }
+
     private static void printPermutation() {
-        System.out.println(String.join(" ",permutations));
+        System.out.println(String.join(" ",elements));
     }
 }
