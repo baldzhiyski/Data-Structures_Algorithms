de9846efaa66bcd702170ab3c5e7ef769ccc53ee
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-27 14:05:15 +0300

    Impl Combinations without Rep

---
+++ SoftUni_Course/Algorithms/Combinatorial Problems/src/CombinationsWithoutRep.java
@@ -0,0 +1,28 @@
+public class CombinationsWithoutRep {
+    static String[] elements = {"A", "B", "C","D"};
+    static  int k = 3;
+
+    static String[] variations = new String[k];
+
+    public static void main(String[] args) {
+        combinations(0,0);
+    }
+
+    private static void combinations(int index, int start) {
+        if(index==variations.length){
+            print(variations);
+        }else{
+            for (int i = start; i < elements.length; i++) {
+                variations[index] = elements[i];
+
+                combinations(index+1, i+1);
+            }
+        }
+    }
+
+    private static void print(String[] arr) {
+        System.out.println(String.join(" ",arr));
+    }
+
+
+}
