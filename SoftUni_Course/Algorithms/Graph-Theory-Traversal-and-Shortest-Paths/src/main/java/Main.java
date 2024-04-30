17e0e84beb0e21198144c84d8f0b4d6fa28c7156
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-04-30 23:30:10 +0300

    Implementing the exercise connected components in graph

--- SoftUni_Course/Algorithms/Graph-Theory-Traversal-and-Shortest-Paths/src/main/java/Main.java
+++ SoftUni_Course/Algorithms/Graph-Theory-Traversal-and-Shortest-Paths/src/main/java/Main.java
@@ -1,17 +1,86 @@
-import java.util.Collection;
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String nextLine = scanner.nextLine();
+
+            if(nextLine.trim().isEmpty()){
+                graph.add(new ArrayList<>());
+                continue;
+            }
+
+            List<Integer> nextNodes = Arrays.stream(nextLine.split("\\s+"))
+                    .map(Integer::parseInt).collect(Collectors.toList());
+
+            graph.add(nextNodes);
+        }
+        List<Deque<Integer>> components = getConnectedComponents(graph);
+        printComponents(components);
+    }
+
+    private static void printComponents(List<Deque<Integer>> components) {
+        for (Deque<Integer> component : components) {
+            System.out.println("Conected component: ");
+            for (Integer integer : component) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
-        throw new AssertionError("Not Implemented");
+        boolean[] visited = new boolean[graph.size()];
+        List<Deque<Integer>> components = new ArrayList<>();
+
+        for (int i = 0; i < graph.size(); i++) {
+            if(!visited[i]){
+                components.add(new ArrayDeque<>());
+                dfs(i,components,graph,visited);
+            }
+        }
+
+
+
+        return components;
     }
 
+    private static void dfs(int node, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+        if(!visited[node]){
+            visited[node] = true;
+
+            for (Integer child : graph.get(node)) {
+                dfs(child,components,graph,visited);
+            }
+            components.get(components.size()-1).offer(node);
+        }
+    }
+
+    private static void bfs(int start, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        visited[start] = true;
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            components.get(components.size() - 1).offer(node);
+
+            for (int child : graph.get(node)) {
+                if (!visited[child]) {
+                    visited[child] = true;
+                    queue.offer(child);
+                }
+            }
+        }
+    }
     public static Collection<String> topSort(Map<String, List<String>> graph) {
         throw new AssertionError("Not Implemented");
     }
