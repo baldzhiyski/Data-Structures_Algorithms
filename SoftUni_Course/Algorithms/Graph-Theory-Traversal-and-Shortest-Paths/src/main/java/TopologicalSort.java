6a595a4f94a17611edde70c09f51afb59520fb43
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-05-01 00:20:02 +0300

    Implementing TopologicalSort via DFS

--- SoftUni_Course/Algorithms/Graph-Theory-Traversal-and-Shortest-Paths/src/main/java/TopologicalSort.java
+++ SoftUni_Course/Algorithms/Graph-Theory-Traversal-and-Shortest-Paths/src/main/java/TopologicalSort.java
@@ -4,6 +4,39 @@ public class TopologicalSort {
     public static void main(String[] args) {
 
     }
+
+    public static Collection<String> topSortRec(Map<String, List<String>> graph) {
+        List<String> sorted = new ArrayList<>();
+
+        Set<String> visited = new HashSet<>();
+        Set<String> detectCycles = new HashSet<>();
+
+        for (Map.Entry<String, List<String>> node : graph.entrySet()) {
+            dfs(node.getKey(),graph,visited,sorted,detectCycles);
+        }
+
+        Collections.reverse(sorted);
+
+        return sorted;
+    }
+
+    private static void dfs(String key, Map<String, List<String>> graph, Set<String> visited, List<String> sorted, Set<String> detectCycles) {
+        if(detectCycles.contains(key)){
+            throw new IllegalArgumentException();
+        }
+        if (!visited.contains(key)) {
+            visited.add(key);
+            detectCycles.add(key); // Add to detectCycles before exploring children
+
+            for (String child : graph.getOrDefault(key, Collections.emptyList())) {
+                dfs(child, graph, visited, sorted, detectCycles);
+            }
+
+            sorted.add(key); // Add to sorted after visiting all children
+            detectCycles.remove(key); // Remove from detectCycles after DFS completes
+        }
+    }
+
     public static Collection<String> topSort(Map<String, List<String>> graph) {
         Map<String,Integer> dependenciesCount = getDepCount(graph);
 
