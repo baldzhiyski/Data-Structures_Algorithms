8925366445cd08fb8b73878eb6ce8ca3f88bbfbf
baldzhiyski__ <hristo_baldjiiski04@abv.bg>
2024-05-01 19:04:48 +0300

    Adding more logic so it can work with not sorted input

--- SoftUni_Course/Algorithms/Graph-Theory-Traversal-and-Shortest-Paths/src/main/java/DistanceBetweenVerticesSecond.java
+++ SoftUni_Course/Algorithms/Graph-Theory-Traversal-and-Shortest-Paths/src/main/java/DistanceBetweenVerticesSecond.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class DistanceBetweenVerticesSecond {
     public static int[][] graph;
 
+    public static Map<Integer,Integer> indexMapper = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,6 +15,9 @@ public class DistanceBetweenVerticesSecond {
 
         for (int i = 1; i <= nodes; i++) {
             String[] edges = scanner.nextLine().split(":");
+
+            indexMapper.put(Integer.parseInt(edges[0]),i);
+
             if (edges.length == 1) {
                 graph[i] = new int[0];
             } else {
@@ -31,11 +36,11 @@ public class DistanceBetweenVerticesSecond {
             int[] prev = new int[graph.length];
             Arrays.fill(prev, -1);
 
-            bfsTraversal(source, destination, prev);
+            bfsTraversal(indexMapper.get(source), indexMapper.get(destination), prev);
 
             List<Integer> path = new ArrayList<>();
 
-            int parent = prev[destination];
+            int parent = prev[indexMapper.get(destination)];
             while (parent != -1) {
                 path.add(parent);
                 parent = prev[parent];
@@ -47,6 +52,8 @@ public class DistanceBetweenVerticesSecond {
 
     private static void bfsTraversal(int source, int destination, int[] prev) {
         Deque<Integer> queue = new ArrayDeque<>();
+
+
         queue.offer(source);
         boolean[] visited = new boolean[graph.length];
         visited[source] = true;
@@ -56,7 +63,8 @@ public class DistanceBetweenVerticesSecond {
 
             if (node == destination) return;
 
-            for (int child : graph[node]) {
+            for (int i = 0; i<graph[node].length; i++) {
+                int child = indexMapper.get(graph[node][i]);
                 if (!visited[child]) {
                     prev[child] = node;
                     visited[child] = true;
